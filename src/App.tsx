9566634d21fff1a6e376a06d6465036f1a3fb2a3
WizardRabbit <curious.wzrabbit@gmail.com>
2024-06-10 22:29:35 +0900

    ✨ feat: 전체적인 구동에 필요한 useApp 커스텀 훅, App 컴포넌트 구현
    - 리팩터링이 완료된 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
+import UserVideoComponent from "./components/UserVideoComponent";
 import useApp from "./useApp";
 
 const App = () => {
   const {
     username,
     mySessionId,
+    session,
+    mainStreamManager,
+    publisher,
+    subscribers,
+    currentVideoDevice,
     handleChangeUsername,
     handleChangeMySessionId,
+    joinSession,
+    leaveSession,
   } = useApp();
 
-  return (
+  return session ? (
+    <>
+      <h1>연결 완료</h1>
+      <button type="button" onClick={leaveSession}>
+        접속 종료
+      </button>
+      <h2>메인 스트림 매니저</h2>
+      {mainStreamManager && (
+        <UserVideoComponent streamManager={mainStreamManager} />
+      )}
+      <h2>퍼블리셔</h2>
+      {publisher && <UserVideoComponent streamManager={publisher} />}
+      <h2>다른 참가자들</h2>
+      {subscribers.map((subscriber) => (
+        <div key={subscriber.id}>
+          <span>{subscriber.id}</span>
+          <UserVideoComponent streamManager={subscriber} />
+        </div>
+      ))}
+    </>
+  ) : (
     <>
       <h1>OpenVidu 화상 채팅 애플리케이션 테스트</h1>
       <p>사용자명</p>
@@ -16,7 +44,9 @@ const App = () => {
       <p>세션 아이디</p>
       <input value={mySessionId} onChange={handleChangeMySessionId} required />
       <br />
-      <button type="button">접속하기!</button>
+      <button type="button" onClick={joinSession}>
+        접속하기!
+      </button>
     </>
   );
 };
--- src/useApp.tsx
+++ src/useApp.tsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import type { ChangeEventHandler } from "react";
+import getToken from "./domains/getToken";
+import { OpenVidu } from "openvidu-browser";
 
 const useApp = () => {
   const [username, setUsername] = useState("");
   const [mySessionId, setMySessionId] = useState("");
+  const [session, setSession] = useState<any>(undefined);
+  const [mainStreamManager, setMainStreamManager] = useState<any>(undefined);
+  const [publisher, setPublisher] = useState<any>(undefined);
+  const [subscribers, setSubscribers] = useState<any[]>([]);
+  const [currentVideoDevice, setCurrentVideoDevice] = useState<any>(undefined);
+
+  const openVidu = useRef<OpenVidu | null>();
 
   const handleChangeUsername: ChangeEventHandler<HTMLInputElement> = (
     event
@@ -17,11 +26,109 @@ const useApp = () => {
     setMySessionId(event.target.value);
   };
 
+  const joinSession = async () => {
+    openVidu.current = new OpenVidu();
+    const newSession = openVidu.current.initSession();
+
+    newSession.on("streamCreated", (event) => {
+      const subscriber = newSession.subscribe(event.stream, undefined);
+      setSubscribers((subscribers) => [...subscribers, subscriber]);
+    });
+
+    newSession.on("streamDestroyed", (event) => {
+      setSubscribers((subscribers) =>
+        subscribers.filter(
+          (subscriber) =>
+            subscriber.stream.streamManager !== event.stream.streamManager
+        )
+      );
+    });
+
+    newSession.on("exception", (exception) => {
+      console.warn(`예외 발생: ${exception}`);
+    });
+
+    const token = await getToken(mySessionId);
+
+    newSession
+      .connect(token, { clientData: username })
+      .then(async () => {
+        const publisher = await openVidu.current?.initPublisherAsync(
+          undefined,
+          {
+            audioSource: undefined,
+            videoSource: undefined,
+            publishAudio: true,
+            publishVideo: true,
+            resolution: "640x480",
+            frameRate: 30,
+            insertMode: "APPEND",
+            mirror: false,
+          }
+        );
+
+        if (!publisher) {
+          return;
+        }
+
+        newSession.publish(publisher);
+
+        const devices = await openVidu.current?.getDevices();
+        const videoDevices = devices?.filter(
+          (device) => device.kind === "videoinput"
+        );
+        const currentVideoDeviceId = publisher.stream
+          .getMediaStream()
+          .getVideoTracks()[0]
+          .getSettings().deviceId;
+        const currentVideoDevice = videoDevices?.find(
+          (device) => device.deviceId === currentVideoDeviceId
+        );
+
+        setCurrentVideoDevice(currentVideoDevice);
+        setMainStreamManager(publisher);
+        setPublisher(publisher);
+      })
+      .catch((error) => {
+        console.log("세션 연결에 실패했습니다.", error.code, error.message);
+      });
+
+    setSession(newSession);
+  };
+
+  const leaveSession = useCallback(() => {
+    if (session) {
+      session.disconnect();
+    }
+
+    openVidu.current = null;
+
+    setSession(undefined);
+    setSubscribers([]);
+    setMainStreamManager(undefined);
+    setPublisher(undefined);
+  }, [session]);
+
+  useEffect(() => {
+    document.addEventListener("beforeunload", leaveSession);
+
+    return () => {
+      document.removeEventListener("beforeunload", leaveSession);
+    };
+  }, [leaveSession]);
+
   return {
     username,
     mySessionId,
+    session,
+    mainStreamManager,
+    publisher,
+    subscribers,
+    currentVideoDevice,
     handleChangeUsername,
     handleChangeMySessionId,
+    joinSession,
+    leaveSession,
   };
 };
 
