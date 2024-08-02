cbbc95f8766f0e0f4c21ab06a300363a6179dd35
WizardRabbit <curious.wzrabbit@gmail.com>
2024-08-03 00:30:59 +0900

    ✨ feat: 캠 켜기/끄기 기능을 추가하고, 참가자가 말하고 있을 경우 녹색으로 강조하는 기능 추가

--- src/components/OvVideo.tsx
+++ src/components/OvVideo.tsx
@@ -1,5 +1,8 @@
-import { useRef, useEffect } from "react";
-import type { StreamManager } from "openvidu-browser";
+import { useState, useRef, useEffect } from "react";
+import type {
+  StreamManager,
+  StreamPropertyChangedEvent,
+} from "openvidu-browser";
 
 interface OpenViduVideoComponentProps {
   streamManager: StreamManager;
@@ -8,7 +11,38 @@ interface OpenViduVideoComponentProps {
 const OpenViduVideoComponent = ({
   streamManager,
 }: OpenViduVideoComponentProps) => {
+  const [isVideoActive, setIsVideoActive] = useState(
+    streamManager.stream.videoActive
+  );
+  const [isUserSpeaking, setIsUserSpeaking] = useState(false);
   const videoRef = useRef(null);
+
+  useEffect(() => {
+    const updateVideoActive = (event: StreamPropertyChangedEvent) => {
+      const videoActive = event.stream.videoActive;
+
+      if (event.changedProperty === "videoActive") {
+        setIsVideoActive(videoActive);
+      }
+    };
+
+    const updateStartSpeaking = () => {
+      setIsUserSpeaking(true);
+    };
+
+    const updateStopSpeaking = () => {
+      setIsUserSpeaking(false);
+    };
+
+    streamManager.on("streamPropertyChanged", updateVideoActive);
+    streamManager.on("publisherStartSpeaking", updateStartSpeaking);
+    streamManager.on("publisherStopSpeaking", updateStopSpeaking);
+    return () => {
+      streamManager.off("streamPropertyChanged", updateVideoActive);
+      streamManager.off("publisherStartSpeaking", updateStartSpeaking);
+      streamManager.off("publisherStopSpeaking", updateStopSpeaking);
+    };
+  }, [streamManager]);
 
   useEffect(() => {
     const $videoElement = videoRef.current;
@@ -20,7 +54,24 @@ const OpenViduVideoComponent = ({
     streamManager.addVideoElement($videoElement);
   }, [streamManager]);
 
-  return <video autoPlay={true} ref={videoRef} />;
+  return (
+    <>
+      <video
+        autoPlay={true}
+        ref={videoRef}
+        style={{
+          boxShadow: `${isUserSpeaking ? "0 0 30px green" : "0 0 0px green"}`,
+          scale: `${isUserSpeaking ? "1.02" : "1"}`,
+          transition: "0.2s",
+        }}
+      />
+      <div>현재 참가자의 상태</div>
+      <ul>
+        <li>{`isVideoActive: ${isVideoActive}`}</li>
+        <li>{`speaking: ${isUserSpeaking}`}</li>
+      </ul>
+    </>
+  );
 };
 
 export default OpenViduVideoComponent;
